0e3fdd7fad95b966958d446117baf07d329eba6d
Icenowy Zheng <uwu@icenowy.me>
2024-06-29 13:22:47 +0800

    drm/ttm: downgrade cached to write_combined when snooping not available
    As we can now acquire the presence of the full DMA coherency (snooping
    capability) from ttm_device, we can now map the CPU side memory as
    write-combined when cached is requested and snooping is not avilable.
    
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>

--- drivers/gpu/drm/ttm/ttm_tt.c
+++ drivers/gpu/drm/ttm/ttm_tt.c
@@ -152,6 +152,10 @@ static void ttm_tt_init_fields(struct ttm_tt *ttm,
 			       enum ttm_caching caching,
 			       unsigned long extra_pages)
 {
+	/* Downgrade cached mapping for non-snooping devices */
+	if (!bo->bdev->dma_coherent && caching == ttm_cached)
+		caching = ttm_write_combined;
+
 	ttm->num_pages = (PAGE_ALIGN(bo->base.size) >> PAGE_SHIFT) + extra_pages;
 	ttm->page_flags = page_flags;
 	ttm->dma_address = NULL;
