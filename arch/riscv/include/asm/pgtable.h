4b9309099916747440c06a43e015f88207880645
Samuel Holland <samuel.holland@sifive.com>
2024-10-30 18:46:03 -0700

    riscv: mm: Fix up memory types when writing page tables
    Currently, Linux on RISC-V has three ways to specify the cacheability
    and ordering PMAs of a page:
     1) Do nothing; assume the system is entirely cache-coherent and rely on
        the hardware for any ordering requirements
     2) Use the page table bits specified by Svpbmt
     3) Use the page table bits specified by XTheadMae
    
    To support all three methods, the kernel dynamically determines the
    definitions of the _PAGE_NOCACHE and _PAGE_IO fields. However, this
    alone is not sufficient, as XTheadMae uses a nonzero memory type value
    for normal memory pages. So the kernel has an additional alternative
    sequence (ALT_THEAD_PMA) to insert the correct memory type when writing
    page table entries.
    
    Some RISC-V platforms use a fourth method to specify the cacheability of
    a page of RAM: RAM is mapped to multiple physical address ranges, with
    each alias having a different set of statically-determined PMAs.
    Software selects the PMAs for a page by choosing a PFN from the
    corresponding physical address range. This strategy also requires
    applying a transformation when writing page table entries. Since these
    physical memory aliases should be invisible to the rest of the kernel,
    the opposite transformation must be applied when reading page table
    entries.
    
    However, with this last method of specifying PMAs, there is no inherent
    way to indicate the cacheability of a page in the pgprot_t value, since
    the PFN itself determines cacheability. One possible way is to reuse the
    PTE bits from Svpbmt, as Svpbmt is the standard extension. This requires
    the Svpbmt version of _PAGE_NOCACHE and _PAGE_IO to be available even
    when the CPU does not support the extension.
    
    It turns out that with some clever bit manipulation, it is just as
    efficient to transform all three Svpbmt memory type values to the
    corresponding XTheadMae values, as it is to check for and insert the
    one XTheadMae memory type value for normal memory. This allows the
    _PAGE_NOCACHE and _PAGE_IO definitions to be compile-time constants,
    and it centralizes all memory type handling to one set of ALTERNATIVE
    macros.
    
    For a kernel with both Svpbmt and XTheadMae enabled, this change reduces
    both kernel text size and the number of alternatives applied at boot.
    However, there are a couple of small costs. For platforms using the
    first method ("do nothing"), we must mask off the memory type bits when
    writing page table entries, whereas previously no action was needed.
    Second, when reading page table entries, the XTheadMae values must be
    transformed back to the Svpbmt values. This "unfix" operation is also
    needed for alias-based PMA selection, so both methods can use the same
    ALTERNATIVE.
    
    As a side effect, this change fixes the reporting of the NAPOT and
    memory type bits from ptdump on platforms with XTheadMae.
    
    Signed-off-by: Samuel Holland <samuel.holland@sifive.com>

--- arch/riscv/include/asm/pgtable.h
+++ arch/riscv/include/asm/pgtable.h
@@ -257,11 +257,11 @@ static inline void pmd_clear(pmd_t *pmdp)
 
 static inline pgd_t pfn_pgd(unsigned long pfn, pgprot_t prot)
 {
-	unsigned long prot_val = pgprot_val(prot);
+	pgd_t pgd = __pgd((pfn << _PAGE_PFN_SHIFT) | pgprot_val(prot));
 
-	ALT_THEAD_PMA(prot_val);
+	ALT_FIXUP_MT(pgd);
 
-	return __pgd((pfn << _PAGE_PFN_SHIFT) | prot_val);
+	return pgd;
 }
 
 static inline unsigned long pmd_pfn(pmd_t pmd)
@@ -325,7 +325,10 @@ static inline unsigned long pte_napot(pte_t pte)
 /* Yields the page frame number (PFN) of a page table entry */
 static inline unsigned long pte_pfn(pte_t pte)
 {
-	unsigned long res  = __page_val_to_pfn(pte_val(pte));
+	unsigned long res;
+
+	ALT_UNFIX_MT(pte);
+	res  = __page_val_to_pfn(pte_val(pte));
 
 	if (has_svnapot() && pte_napot(pte))
 		res = res & (res - 1UL);
@@ -338,11 +341,11 @@ static inline unsigned long pte_pfn(pte_t pte)
 /* Constructs a page table entry */
 static inline pte_t pfn_pte(unsigned long pfn, pgprot_t prot)
 {
-	unsigned long prot_val = pgprot_val(prot);
+	pte_t pte = __pte((pfn << _PAGE_PFN_SHIFT) | pgprot_val(prot));
 
-	ALT_THEAD_PMA(prot_val);
+	ALT_FIXUP_MT(pte);
 
-	return __pte((pfn << _PAGE_PFN_SHIFT) | prot_val);
+	return pte;
 }
 
 #define mk_pte(page, prot)       pfn_pte(page_to_pfn(page), prot)
@@ -489,9 +492,11 @@ static inline pte_t pte_modify(pte_t pte, pgprot_t newprot)
 {
 	unsigned long newprot_val = pgprot_val(newprot);
 
-	ALT_THEAD_PMA(newprot_val);
+	ALT_UNFIX_MT(pte);
+	pte = __pte((pte_val(pte) & _PAGE_CHG_MASK) | newprot_val);
+	ALT_FIXUP_MT(pte);
 
-	return __pte((pte_val(pte) & _PAGE_CHG_MASK) | newprot_val);
+	return pte;
 }
 
 #define pgd_ERROR(e) \
