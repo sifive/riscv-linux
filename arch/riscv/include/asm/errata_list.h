dfb16cfabca3307249cbf4c4c40794a705da8422
Samuel Holland <samuel.holland@sifive.com>
2024-10-30 18:46:03 -0700

    riscv: mm: Fix up memory types when writing page tables
    Currently, Linux on RISC-V has three ways to specify the cacheability
    and ordering PMAs of a page:
     1) Do nothing; assume the system is entirely cache-coherent and rely on
        the hardware for any ordering requirements
     2) Use the page table bits specified by Svpbmt
     3) Use the page table bits specified by XTheadMae
    
    To support all three methods, the kernel dynamically determines the
    definitions of the _PAGE_NOCACHE and _PAGE_IO fields. However, this
    alone is not sufficient, as XTheadMae uses a nonzero memory type value
    for normal memory pages. So the kernel has an additional alternative
    sequence (ALT_THEAD_PMA) to insert the correct memory type when writing
    page table entries.
    
    Some RISC-V platforms use a fourth method to specify the cacheability of
    a page of RAM: RAM is mapped to multiple physical address ranges, with
    each alias having a different set of statically-determined PMAs.
    Software selects the PMAs for a page by choosing a PFN from the
    corresponding physical address range. This strategy also requires
    applying a transformation when writing page table entries. Since these
    physical memory aliases should be invisible to the rest of the kernel,
    the opposite transformation must be applied when reading page table
    entries.
    
    However, with this last method of specifying PMAs, there is no inherent
    way to indicate the cacheability of a page in the pgprot_t value, since
    the PFN itself determines cacheability. One possible way is to reuse the
    PTE bits from Svpbmt, as Svpbmt is the standard extension. This requires
    the Svpbmt version of _PAGE_NOCACHE and _PAGE_IO to be available even
    when the CPU does not support the extension.
    
    It turns out that with some clever bit manipulation, it is just as
    efficient to transform all three Svpbmt memory type values to the
    corresponding XTheadMae values, as it is to check for and insert the
    one XTheadMae memory type value for normal memory. This allows the
    _PAGE_NOCACHE and _PAGE_IO definitions to be compile-time constants,
    and it centralizes all memory type handling to one set of ALTERNATIVE
    macros.
    
    For a kernel with both Svpbmt and XTheadMae enabled, this change reduces
    both kernel text size and the number of alternatives applied at boot.
    However, there are a couple of small costs. For platforms using the
    first method ("do nothing"), we must mask off the memory type bits when
    writing page table entries, whereas previously no action was needed.
    Second, when reading page table entries, the XTheadMae values must be
    transformed back to the Svpbmt values. This "unfix" operation is also
    needed for alias-based PMA selection, so both methods can use the same
    ALTERNATIVE.
    
    As a side effect, this change fixes the reporting of the NAPOT and
    memory type bits from ptdump on platforms with XTheadMae.
    
    Signed-off-by: Samuel Holland <samuel.holland@sifive.com>

--- arch/riscv/include/asm/errata_list.h
+++ arch/riscv/include/asm/errata_list.h
@@ -59,49 +59,32 @@ asm(ALTERNATIVE("sfence.vma %0, %1", "sfence.vma", SIFIVE_VENDOR_ID,	\
 		: : "r" (addr), "r" (asid) : "memory")
 
 /*
- * _val is marked as "will be overwritten", so need to set it to 0
- * in the default case.
+ * dcache.ipa rs1 (invalidate, physical address)
+ * | 31 - 25 | 24 - 20 | 19 - 15 | 14 - 12 | 11 - 7 | 6 - 0 |
+ *   0000001    01010      rs1       000      00000  0001011
+ * dache.iva rs1 (invalida, virtual address)
+ *   0000001    00110      rs1       000      00000  0001011
+ *
+ * dcache.cpa rs1 (clean, physical address)
+ * | 31 - 25 | 24 - 20 | 19 - 15 | 14 - 12 | 11 - 7 | 6 - 0 |
+ *   0000001    01001      rs1       000      00000  0001011
+ * dcache.cva rs1 (clean, virtual address)
+ *   0000001    00101      rs1       000      00000  0001011
+ *
+ * dcache.cipa rs1 (clean then invalidate, physical address)
+ * | 31 - 25 | 24 - 20 | 19 - 15 | 14 - 12 | 11 - 7 | 6 - 0 |
+ *   0000001    01011      rs1       000      00000  0001011
+ * dcache.civa rs1 (... virtual address)
+ *   0000001    00111      rs1       000      00000  0001011
+ *
+ * sync.s (make sure all cache operations finished)
+ * | 31 - 25 | 24 - 20 | 19 - 15 | 14 - 12 | 11 - 7 | 6 - 0 |
+ *   0000000    11001     00000      000      00000  0001011
  */
-#define ALT_SVPBMT_SHIFT 61
-#define ALT_THEAD_MAE_SHIFT 59
-#define ALT_SVPBMT(_val, prot)						\
-asm(ALTERNATIVE_2("li %0, 0\t\nnop",					\
-		  "li %0, %1\t\nslli %0,%0,%3", 0,			\
-			RISCV_ISA_EXT_SVPBMT, CONFIG_RISCV_ISA_SVPBMT,	\
-		  "li %0, %2\t\nslli %0,%0,%4", THEAD_VENDOR_ID,	\
-			ERRATA_THEAD_MAE, CONFIG_ERRATA_THEAD_MAE)	\
-		: "=r"(_val)						\
-		: "I"(prot##_SVPBMT >> ALT_SVPBMT_SHIFT),		\
-		  "I"(prot##_THEAD >> ALT_THEAD_MAE_SHIFT),		\
-		  "I"(ALT_SVPBMT_SHIFT),				\
-		  "I"(ALT_THEAD_MAE_SHIFT))
-
-#ifdef CONFIG_ERRATA_THEAD_MAE
-/*
- * IO/NOCACHE memory types are handled together with svpbmt,
- * so on T-Head chips, check if no other memory type is set,
- * and set the non-0 PMA type if applicable.
- */
-#define ALT_THEAD_PMA(_val)						\
-asm volatile(ALTERNATIVE(						\
-	__nops(7),							\
-	"li      t3, %1\n\t"						\
-	"slli    t3, t3, %3\n\t"					\
-	"and     t3, %0, t3\n\t"					\
-	"bne     t3, zero, 2f\n\t"					\
-	"li      t3, %2\n\t"						\
-	"slli    t3, t3, %3\n\t"					\
-	"or      %0, %0, t3\n\t"					\
-	"2:",  THEAD_VENDOR_ID,						\
-		ERRATA_THEAD_MAE, CONFIG_ERRATA_THEAD_MAE)		\
-	: "+r"(_val)							\
-	: "I"(_PAGE_MTMASK_THEAD >> ALT_THEAD_MAE_SHIFT),		\
-	  "I"(_PAGE_PMA_THEAD >> ALT_THEAD_MAE_SHIFT),			\
-	  "I"(ALT_THEAD_MAE_SHIFT)					\
-	: "t3")
-#else
-#define ALT_THEAD_PMA(_val)
-#endif
+#define THEAD_inval_A0	".long 0x0265000b"
+#define THEAD_clean_A0	".long 0x0255000b"
+#define THEAD_flush_A0	".long 0x0275000b"
+#define THEAD_SYNC_S	".long 0x0190000b"
 
 #define ALT_CMO_OP(_op, _start, _size, _cachesize)			\
 asm volatile(ALTERNATIVE(						\
