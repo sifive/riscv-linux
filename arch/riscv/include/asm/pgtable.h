4a754d92458f0261e56c4d615a72685934cb319e
Samuel Holland <samuel.holland@sifive.com>
2024-10-31 19:33:42 -0700

    riscv: mm: Use physical memory aliases to apply PMAs
    On some RISC-V platforms, RAM is mapped to multiple physical address
    ranges, with each alias having a different set of statically-determined
    Physical Memory Attributes (PMAs). Software selects the PMAs for a page
    by choosing a PFN from the corresponding physical address range.
    
    Implement this by transforming the PFN when writing page tables.  If the
    memory type field is nonzero, replace the PFN with the corresponding PFN
    from the noncached alias. Similarly, when reading from the page tables,
    if the PFN is found in a noncached alias, replace it with the
    corresponding PFN from the cached alias, and insert _PAGE_NOCACHE.
    
    The rest of the kernel sees only the cached PFNs and _PAGE_MTMASK values
    as if Svpbmt was implemented.
    
    Memory alias pairs are determined from the devicetree. A new cpufeature
    bit is required because that is the only way to trigger alternative
    patching.
    
    Signed-off-by: Samuel Holland <samuel.holland@sifive.com>

--- arch/riscv/include/asm/pgtable.h
+++ arch/riscv/include/asm/pgtable.h
@@ -952,6 +952,14 @@ extern u64 satp_mode;
 void paging_init(void);
 void misc_mem_init(void);
 
+#ifdef CONFIG_RISCV_ISA_SVPBMT
+bool __init riscv_have_memory_alias(void);
+void __init riscv_init_memory_alias(void);
+#else
+static inline bool riscv_have_memory_alias(void) { return false; }
+static inline void riscv_init_memory_alias(void) {}
+#endif /* CONFIG_RISCV_ISA_SVPBMT */
+
 /*
  * ZERO_PAGE is a global shared page that is always zero,
  * used for zero-mapped memory areas, etc.
