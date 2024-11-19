28d21c0d67823a695a033d2eb9d2e59e30d6dadd
Pritesh Patel <pritesh.patel@einfochips.com>
2024-11-19 10:01:51 +0000

    drivers: pcie: eic7700: Implement interrupt-map for PCIe
    - Optimize PCIe driver
    - Added "interrupt-map" property in dts to avoid below boot time
      error
    
    Error Logs
    -----------
    amdgpu 0000:01:00.0: Failed to enable PASID
    amdgpu 0000:01:00.0: of_irq_parse_pci: failed with rc=-22
    
    Signed-off-by: Pritesh Patel <pritesh.patel@einfochips.com>

--- drivers/pci/controller/dwc/pcie-eswin.c
+++ drivers/pci/controller/dwc/pcie-eswin.c
@@ -40,9 +40,6 @@
 #include <linux/property.h>
 #include "pcie-designware.h"
 
-#define _writel_relaxed(v, p) 	writel_relaxed(v, p)
-#define to_eswin_pcie(x)	dev_get_drvdata((x)->dev)
-
 struct eswin_pcie {
 	struct dw_pcie pci;
 	void __iomem *mgmt_base;
@@ -54,30 +51,23 @@ struct eswin_pcie {
 	struct reset_control *powerup_rst;
 	struct reset_control *cfg_rst;
 	struct reset_control *perst;
-	int gen_x;
-	int lane_x;
 };
 
-#define PCIEMGMT_ACLK_CTRL		0x170
-#define PCIEMGMT_ACLK_CLKEN		BIT(31)
-#define PCIEMGMT_XTAL_SEL		BIT(20)
-#define PCIEMGMT_DIVSOR		0xf0
-
-#define PCIEMGMT_CFG_CTRL		0x174
-#define PCIEMGMT_CFG_CLKEN		BIT(31)
-#define PCIEMGMT_AUX_CLKEN		BIT(1)
-#define PCIEMGMT_CR_CLKEN		BIT(0)
-
-#define PCIEMGMT_RST_CTRL		0x420
-#define PCIEMGMT_PERST_N		BIT(2)
-#define PCIEMGMT_POWERUP_RST_N	BIT(1)
-#define PCIEMGMT_CFG_RST_N		BIT(0)
-
 #define PCIE_PM_SEL_AUX_CLK		BIT(16)
-
 #define PCIEMGMT_APP_HOLD_PHY_RST	BIT(6)
 #define PCIEMGMT_APP_LTSSM_ENABLE	BIT(5)
 #define PCIEMGMT_DEVICE_TYPE_MASK	0xf
+
+#define PCIEMGMT_CTRL0_OFFSET	0x0
+#define PCIEMGMT_STATUS0_OFFSET	0x100
+
+#define PCIE_TYPE_DEV_VEND_ID	0x0
+#define PCIE_DSP_PF0_MSI_CAP	0x50
+#define PCIE_NEXT_CAP_PTR		0x70
+#define DEVICE_CONTROL_DEVICE_STATUS	0x78
+
+#define PCIE_MSI_MULTIPLE_MSG_32	(0x5<<17)
+#define PCIE_MSI_MULTIPLE_MSG_MASK	(0x7<<17)
 
 #define PCIEMGMT_LINKUP_STATE_VALIDATE  ((0x11<<2)|0x3)
 #define PCIEMGMT_LINKUP_STATE_MASK      0xff
@@ -97,9 +87,9 @@ static int eswin_pcie_start_link(struct dw_pcie *pci)
 	u32 val;
 
 	/* Enable LTSSM */
-	val = readl_relaxed(pcie->mgmt_base);
+	val = readl_relaxed(pcie->mgmt_base + PCIEMGMT_CTRL0_OFFSET);
 	val |= PCIEMGMT_APP_LTSSM_ENABLE;
-	_writel_relaxed(val, pcie->mgmt_base);
+	writel_relaxed(val, pcie->mgmt_base + PCIEMGMT_CTRL0_OFFSET);
 	return 0;
 }
 
@@ -109,7 +99,7 @@ static int eswin_pcie_link_up(struct dw_pcie *pci)
 	struct eswin_pcie *pcie = dev_get_drvdata(dev);
 	u32 val;
 
-	val = readl_relaxed(pcie->mgmt_base + 0x100);
+	val = readl_relaxed(pcie->mgmt_base + PCIEMGMT_STATUS0_OFFSET);
 	if ((val & PCIEMGMT_LINKUP_STATE_MASK) == PCIEMGMT_LINKUP_STATE_VALIDATE)
 		return 1;
 	else
@@ -183,9 +173,6 @@ static int eswin_pcie_power_off(struct eswin_pcie *eswin_pcie)
 	return 0;
 }
 
-/* TODO: Check socket gpio number in actual board and
- *       change if necessary
- */
 int eswin_evb_socket_power_on(struct device *dev)
 {
 	int err_desc=0;
@@ -194,7 +181,7 @@ int eswin_evb_socket_power_on(struct device *dev)
 	err_desc = IS_ERR(gpio);
 
 	if (err_desc) {
-		pr_err("No power control gpio found, maybe not needed\n");
+		pr_debug("No power control gpio found, maybe not needed\n");
 		return 0;
 	}
 
@@ -206,7 +193,7 @@ int eswin_evb_socket_power_on(struct device *dev)
 static int eswin_pcie_host_init(struct dw_pcie_rp *pp)
 {
 	struct dw_pcie *pci = to_dw_pcie_from_pp(pp);
-	struct eswin_pcie *pcie = to_eswin_pcie(pci);
+	struct eswin_pcie *pcie = dev_get_drvdata(pci->dev);
 	int ret;
 	u32 val;
 
@@ -224,9 +211,9 @@ static int eswin_pcie_host_init(struct dw_pcie_rp *pp)
 		return ret;
 
 	/* set device type : rc */
-	val = readl_relaxed(pcie->mgmt_base);
+	val = readl_relaxed(pcie->mgmt_base + PCIEMGMT_CTRL0_OFFSET);
 	val &= 0xfffffff0;
-	_writel_relaxed(val|0x4, pcie->mgmt_base);
+	writel_relaxed(val|0x4, pcie->mgmt_base + PCIEMGMT_CTRL0_OFFSET);
 
 	ret = reset_control_assert(pcie->perst);
 	WARN_ON(0 != ret);
@@ -237,92 +224,43 @@ static int eswin_pcie_host_init(struct dw_pcie_rp *pp)
 	WARN_ON(0 != ret);
 
 	/* app_hold_phy_rst */
-	val = readl_relaxed(pcie->mgmt_base);
+	val = readl_relaxed(pcie->mgmt_base + PCIEMGMT_CTRL0_OFFSET);
 	val &= ~(0x40);
-	_writel_relaxed(val, pcie->mgmt_base);
+	writel_relaxed(val, pcie->mgmt_base + PCIEMGMT_CTRL0_OFFSET);
 
 	/* wait pm_sel_aux_clk to 0 */
 	while (1) {
-		val = readl_relaxed(pcie->mgmt_base + 0x100);
+		val = readl_relaxed(pcie->mgmt_base + PCIEMGMT_STATUS0_OFFSET);
 		if (!(val & PCIE_PM_SEL_AUX_CLK)) {
 			break;
 		}
 		msleep(1);
 	}
 
-	/* TODO: All hardcoded values such as register addresses and the values written to then
-	 *        to be replaced with macros.
-	 */
-	/* config eswin vendor id and EIC7700 device id */
-	dw_pcie_writel_dbi(pci, 0, 0x20301fe1);
-
-	if (pcie->gen_x == 3) {
-		/* GEN3 */
-		dw_pcie_writel_dbi(pci, 0xa0, 0x00010003);
-
-		/* LINK_CAPABILITIES_REG : PCIE_CAP_BASE + 0xc */
-		val = dw_pcie_readl_dbi(pci, 0x7c);
-		val &= 0xfffffff0;
-		/* GEN3 */
-		val |= 0x3;
-		dw_pcie_writel_dbi(pci, 0x7c, val);
-	} else if (pcie->gen_x == 2) {
-		/* GEN2 */
-		dw_pcie_writel_dbi(pci, 0xa0, 0x00010002);
-
-		/* LINK_CAPABILITIES_REG : PCIE_CAP_BASE + 0xc */
-		val = dw_pcie_readl_dbi(pci, 0x7c);
-		val &= 0xfffffff0;
-		val |= 0x2;
-		dw_pcie_writel_dbi(pci, 0x7c, val);
-	}else {
-		/* GEN1 */
-		dw_pcie_writel_dbi(pci, 0xa0, 0x00010001);
-
-		/* LINK_CAPABILITIES_REG : PCIE_CAP_BASE + 0xc */
-		val = dw_pcie_readl_dbi(pci, 0x7c);
-		val &= 0xfffffff0;
-		val |= 0x1;
-		dw_pcie_writel_dbi(pci, 0x7c, val);
-	}
-
-	/* LINK_CAPABILITIES_REG : PCIE_CAP_BASE + 0xc : laneX */
-	val = dw_pcie_readl_dbi(pci, 0x7c);
-	val &= 0xfffffc0f;
-	if (pcie->lane_x == 4) {
-		val |= 0x40;
-	} else if (pcie->lane_x == 2) {
-		val |= 0x20;
-	} else {
-		val |= 0x10;
-	}
-
-	dw_pcie_writel_dbi(pci, 0x7c, val);
+	/* config eswin vendor id and eic7700 device id */
+	dw_pcie_writel_dbi(pci, PCIE_TYPE_DEV_VEND_ID, 0x20301fe1);
 
 	/* lane fix config, real driver NOT need, default x4 */
-	val = dw_pcie_readl_dbi(pci, 0x8c0);
+	val = dw_pcie_readl_dbi(pci, PCIE_PORT_MULTI_LANE_CTRL);
 	val &= 0xffffff80;
-	if (pcie->lane_x == 4) {
-		val |= 0x44;
-	} else if (pcie->lane_x == 2) {
-		val |= 0x42;
-	} else {
-		val |= 0x41;
-	}
-	dw_pcie_writel_dbi(pci, 0x8c0, val);
+	val |= 0x44;
+	dw_pcie_writel_dbi(pci, PCIE_PORT_MULTI_LANE_CTRL, val);
 
-	/* config msix table size to 0 in RC mode because our RC not support msix */
-	val = dw_pcie_readl_dbi(pci, 0xb0);
-	val &= ~(0x7ff<<16);
-	dw_pcie_writel_dbi(pci, 0xb0, val);
+	val = dw_pcie_readl_dbi(pci, DEVICE_CONTROL_DEVICE_STATUS);
+	val &= ~(0x7<<5);
+	val |= (0x2<<5);
+	dw_pcie_writel_dbi(pci, DEVICE_CONTROL_DEVICE_STATUS, val);
 
 	/*  config support 32 msi vectors */
-	dw_pcie_writel_dbi(pci, 0x50, 0x018a7005);
+	val = dw_pcie_readl_dbi(pci, PCIE_DSP_PF0_MSI_CAP);
+	val &= ~PCIE_MSI_MULTIPLE_MSG_MASK;
+	val |= PCIE_MSI_MULTIPLE_MSG_32;
+	dw_pcie_writel_dbi(pci, PCIE_DSP_PF0_MSI_CAP, val);
 
 	/* disable msix cap */
-	val = dw_pcie_readl_dbi(pci, 0x70);
+	val = dw_pcie_readl_dbi(pci, PCIE_NEXT_CAP_PTR);
 	val &= 0xffff00ff;
-	dw_pcie_writel_dbi(pci, 0x70, val);
+	dw_pcie_writel_dbi(pci, PCIE_NEXT_CAP_PTR, val);
 
 	return 0;
 }
@@ -388,6 +326,7 @@ static int eswin_pcie_probe(struct platform_device *pdev)
 	}
 
 	pcie->pcie_aclk = devm_clk_get(dev, "pcie_aclk");
+
 	if (IS_ERR(pcie->pcie_aclk)) {
 		dev_err(dev, "pcie_aclk clock source missing or invalid\n");
 		return PTR_ERR(pcie->pcie_aclk);
@@ -408,9 +347,6 @@ static int eswin_pcie_probe(struct platform_device *pdev)
 	if (IS_ERR_OR_NULL(pcie->perst)) {
 		dev_err_probe(dev, PTR_ERR(pcie->perst), "unable to get perst\n");
 	}
-
-	device_property_read_u32(&pdev->dev, "gen-x", &pcie->gen_x);
-	device_property_read_u32(&pdev->dev, "lane-x", &pcie->lane_x);
 
 	platform_set_drvdata(pdev, pcie);
 
@@ -436,6 +372,6 @@ static struct platform_driver eswin_pcie_driver = {
 module_platform_driver(eswin_pcie_driver);
 
 MODULE_DEVICE_TABLE(of, eswin_pcie_of_match);
-MODULE_DESCRIPTION("PCIe host controller driver for ESWIN EIC77xx SoCs");
+MODULE_DESCRIPTION("PCIe host controller driver for ESWIN EIC7700 SoCs");
 MODULE_AUTHOR("Ning Yu <ningyu@eswincomputing.com>");
 MODULE_LICENSE("GPL v2");
